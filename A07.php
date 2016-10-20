6b970306fffafa4a120ff4987303025c27e29c71
Roberto jesus Sanchez Camacho <rosaca01@floridauniversitaria.es>
2016-10-20 09:59:20 +0200

    Actividad 07

---
+++ A07.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> A07 </title>
+</head>
+<body>
+	<h1> Tabla de multiplicar</h1>
+	<?php 
+
+	//Asignamos Valores
+		$n=1;
+
+	//Usamos While
+		while ($n <= 10) {
+			$mutiplicar= $n * 2;
+			echo " 2 * $n = $mutiplicar <br>";
+			$n++;
+		}
+
+
+
+
+	 ?>
+</body>
+</html>
